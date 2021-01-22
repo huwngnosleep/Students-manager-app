908d0579fe4cc51e058cf4c0f61498c7f0fb2095
huwngnosleep <thanhhung8210@gmail.com>
2021-01-23 01:13:55 +0700

    ...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const { get } = require('http')
-const port = 5000
+const port = process.env.port || 3000
 const route = require('./routes')
 
 
